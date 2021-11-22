d1735848c3c0173c583126be62a2f953224bc864
mingliang.gao <gaoming971366@163.com>
2021-11-22 22:46:43 +0800

    fix bug: dialog显示问题（Dialog.Component）

--- src/views/SetList.vue
+++ src/views/SetList.vue
@@ -2,6 +2,9 @@
   <h1>个人中心，开发中</h1>
   <div>
     <van-cell title="单元格" is-link value="内容" @click="showDialog()"/>
+    <van-dialog v-model:show="show" title="biaoti" show-cancel-button>
+      <div>123123123</div>
+    </van-dialog>
   </div>
 </template>
 
@@ -10,27 +13,19 @@ import { Dialog, Cell } from 'vant'
 
 export default {
   name: 'SetList',
-  components: {
-    // 'van-dialog': Dialog,
-    'van-cell': Cell
-  },
+  inheritAttrs: false,
   data () {
     return {
-      show: false,
-      title: '123',
-      userName: '',
-      userPass: ''
+      show: false
     }
+  },
+  components: {
+    'van-dialog': Dialog.Component,
+    'van-cell': Cell
   },
   methods: {
     showDialog () {
       this.show = true
-      Dialog.alert({
-        title: '标题',
-        message: '弹窗内容'
-      }).then(() => {
-        // on close
-      })
     }
   }
 }
