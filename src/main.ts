d72fb8be284313ead3d0295b198469293a79075c
mingliang.gao <gaoming971366@163.com>
2021-12-21 21:20:26 +0800

    更换main.ts启动方式

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'default-passive-events'
 
-createApp(App).use(store).use(router).mount('#app')
+// createApp(App).use(store).use(router).mount('#app')
+
+const app = createApp(App)
+app.config.globalProperties.version = 'v.1.0.1'
+app.use(router).mount('#app')
